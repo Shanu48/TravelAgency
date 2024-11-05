857f3e674cafddbd566ab803f38f50c66431147c
Shanu48 <149718849+Shanu48@users.noreply.github.com>
2024-11-05 23:28:49 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,4 @@
+// JavaScript for About Us page
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("About Us page loaded!");
+});
---
+++ script.js
@@ -0,0 +1,4 @@
+// This script can be used for additional interactivity, if needed
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Landing page loaded!");
+});
